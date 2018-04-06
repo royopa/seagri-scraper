e32c1c83a3d9badcebc06e8041e34f894adb3fb9
Rodrigo Prado de Jesus <royopa@gmail.com>
2018-04-06 07:49:20 -0300

    alteracao campo data e processo de busca para pegar ultima pagina

--- main.py
+++ main.py
@@ -6,7 +6,32 @@ import datetime
 from bs4 import BeautifulSoup
 
 
-def get_data_from_site(paginas = []):
+def get_ultima_pagina(ultima_data_base):
+    url = 'http://www.seagri.ba.gov.br/cotacao'
+    session = HTMLSession()
+    today = datetime.date.today()
+    params = {
+        'page': '1',
+        'produto' : '',
+        'praca': '',
+        'tipo': '',
+        'data_inicio': ultima_data_base.strftime('%d/%m/%Y'),
+        'data_final': today.strftime('%d/%m/%Y'),
+        'sort':'asc',
+        'order':'Data'
+    }
+
+    response = session.get(url, params=params)
+    if (response.status_code != 200):
+        print('erro no acesso a página: ', url, params)
+        get_ultima_pagina(ultima_data_base)
+    
+    last_page_link = response.html.find('.pager-last > a:nth-child(1)', first=True)
+    element = last_page_link.html.split('=')
+    return int(element[3].split('&')[0])
+
+
+def get_data_from_site(paginas, ultima_data_base):
     url = 'http://www.seagri.ba.gov.br/cotacao'
     session = HTMLSession()
     today = datetime.date.today()
@@ -19,14 +44,16 @@ def get_data_from_site(paginas = []):
             'produto' : '',
             'praca': '',
             'tipo': '',
-            'data_inicio': '01/01/2010',
-            'data_final': today.strftime('%d/%m/%Y')
+            'data_inicio': ultima_data_base.strftime('%d/%m/%Y'),
+            'data_final': today.strftime('%d/%m/%Y'),
+            'sort':'asc',
+            'order':'Data'
         }
         
         response = session.get(url, params=params)
         if (response.status_code != 200):
             print('erro no acesso a página: ', url, params)
-            get_data_from_site([page])
+            get_data_from_site([page], ultima_data_base)
             continue
 
         soup = BeautifulSoup(response.content, 'lxml')
@@ -35,12 +62,19 @@ def get_data_from_site(paginas = []):
         rows = []
         for row in table.find_all("tr")[1:]:
             td1, td2, td3, td4, td5, td6 = row.find_all("td")
-
-            preco = td6.text.replace('R$ ', '').replace(',', '.').replace('sem cotação', '')
-            data_referencia = datetime.datetime.strptime(td1.text, '%d/%m/%Y')
+            preco = td6.text.replace('R$ ', '').replace('.', '').replace(',', '.').replace('sem cotação', '')
+            data_referencia = datetime.datetime.strptime(td1.text, '%d/%m/%Y').date()
             
+            # pula valores em dólar, visto que não será importado
+            if 'US$' in preco:
+                continue
+
             if preco is '':
                 preco = None
+
+            # só insere datas que ainda não estão na base
+            if ultima_data_base > data_referencia:
+                continue
 
             row = {
                 'dt_referencia': data_referencia,
@@ -56,18 +90,31 @@ def get_data_from_site(paginas = []):
         path_file_base = os.path.join('bases', 'precos_seagri_base.csv')
         for row in rows:
             # faz o append no csv da base
-            with open(path_file_base, 'a', newline='') as baseFile:
+            with open(path_file_base, 'a', newline='', encoding='utf8') as baseFile:
                 fieldnames = ['dt_referencia', 'no_produto', 'no_praca', 'no_tipo', 'no_unidade', 'vr_real']
                 writer = csv.DictWriter(baseFile, fieldnames=fieldnames, delimiter=';', quoting=csv.QUOTE_NONNUMERIC)
                 writer.writerow(row)
-                print(row)
     return True
 
 
+def get_ultima_data_disponivel_base(path_file_base):
+    with open(path_file_base, 'r', encoding='utf8') as f:
+        for row in reversed(list(csv.reader(f))):
+            data = row[0].split(';')[0]
+            if data in ['Data', 'dt_referencia']:
+                return None
+            return datetime.datetime.strptime(data[0:10], '%Y-%m-%d').date()
+
+
 def main():
-    pagina_inicial = 6172
-    paginas = list(range(pagina_inicial, 6173, 1))
-    get_data_from_site(paginas)
+    name_base_file = 'precos_seagri_base.csv'
+    path_file_base = os.path.join('bases', name_base_file)
+    ultima_data_base = get_ultima_data_disponivel_base(path_file_base)
+    print('última data base:', ultima_data_base)
+
+    ultima_pagina = get_ultima_pagina(ultima_data_base)    
+    paginas = list(range(1, ultima_pagina, 1))
+    get_data_from_site(paginas, ultima_data_base)
 
 
 if __name__ == '__main__':
